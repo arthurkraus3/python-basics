7f63e094bbb63e427edff2b42b1e123506f3b62d
Jack Kraus <jackraus62@gmail.com>
2016-07-26 19:36:45 +0000

    third

---
+++ newchallenge.py
@@ -0,0 +1,18 @@
+#two arrays with numbers
+abc = [33, 45, 50, 69, 420]
+dez = [234, 6231, 6943, 1930, 333]
+#join the two arrays in a seperate var
+z = abc+dez 
+#function that prints out
+#"yeah evens" if the nums are even or the number if 1
+#else if the nums are odd, print out yea odds
+for x in z:
+    if x%2 is 0:
+        print "yea even"
+    else:
+        print "yeah odd"
+
+
+&& and
+|| or
+!== not
